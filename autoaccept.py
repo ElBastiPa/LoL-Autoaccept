e2e4986578f3008677abd303b90f7437b3f7a98f
ElBastiPa <lucascamejolopez@gmail.com>
2023-11-12 19:00:35 -0300

    Last Update

--- autoaccept.py
+++ autoaccept.py
@@ -3,7 +3,11 @@ import pyautogui
 import time
 
 while(True):
-	accept = pyautogui.locateOnScreen('accept.png', confidence = 0.7)
+	aceptar = pyautogui.locateOnScreen('accept.png', confidence = 0.7)
+	accept = pyautogui.locateOnScreen('acceptEN.png', confidence = 0.7)
 	if(accept):
 		pyautogui.click(accept)
+		time.sleep(2)
+	if(aceptar):
+		pyautogui.click(aceptar)
 		time.sleep(2)
